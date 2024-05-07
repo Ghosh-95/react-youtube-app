ec7446e4bea2dcfbf11b2c60a447eea44cdb3258
Sushovan <sushovanghosh960@gmail.com>
2024-05-07 21:27:59 +0530

    fix: fixed header and make it responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import youtubeLogo from "../assets/youtube-logo.png"
 
-function HeaderIcons({ icon }) {
+function HeaderButtons({ className, children }) {
     return (
-        <i className={`text-2xl fa-regular ${icon} hover:bg-gray-400 hover:rounded-full w-10 h-10 px-[0.5rem] py-1 cursor-pointer`}></i>
+        <button className={`h-10 text-2xl grid place-items-center w-10 transition-colors rounded-full ${className || 0} hover:bg-gray-400`}>
+            {children}
+        </button>
     )
 }
 
@@ -11,25 +13,31 @@ export default function Header() {
         <header className="py-3 px-5 flex items-center justify-between shadow-xl">
             <figure className="w-24 flex items-center gap-4">
                 <i className="text-xl fa-solid fa-bars"></i>
-                <img src={youtubeLogo} className="w-full" alt="" />
+                <img src={youtubeLogo} className="w-full" alt="youtube logo png image" width={200} />
             </figure>
 
-            <article className="w-[55%] flex items-center gap-5">
-                <section className="w-[85%]">
-                    <input className="w-[90%] font-semibold rounded-l-full p-2 pl-5 border border-gray-300 outline-none" type="search" placeholder="Search" />
-                    <button className=" border border-gray-300 py-[0.5rem] rounded-r-full w-[10%] bg-gray-300 transition-colors hover:bg-gray-400 text-white">
-                        <i className="fa-solid fa-search"></i>
+            <article className="w-[55%] flex items-center flex-shrink-0 gap-5">
+                <form id="search-form" className="md:w-[85%] w-[10%] ml-auto">
+                    <input className="hidden md:inline w-[90%] font-semibold rounded-l-full p-2 pl-5 border border-gray-300 outline-none" type="search" placeholder="Search" />
+                    <button className="md:border border-gray-300 md:py-[0.5rem] rounded-r-full md:w-[10%] md:bg-gray-300 transition-colors md:hover:bg-gray-400 md:text-white">
+                        <i className="md:hover:bg-none hover:bg-gray-300 md:hover:bg-inherit md:p-0 p-4 md:rounded-none rounded-full transition-colors fa-solid fa-search"></i>
                     </button>
-                </section>
-                <div className="h-10 grid place-items-center w-10 rounded-full bg-gray-400">
-                    <i className="fa-solid fa-microphone text-2xl text-white"></i>
-                </div>
+                </form>
+                <HeaderButtons className="bg-gray-300">
+                    <i className="fa-solid fa-microphone text-white"></i>
+                </HeaderButtons>
             </article>
 
             <section className="flex items-center gap-7">
-                <HeaderIcons icon="fa-square-plus" />
-                <HeaderIcons icon="fa-bell" />
-                <HeaderIcons icon="fa-circle-user" />
+                <HeaderButtons>
+                    <i className="fa-regular fa-square-plus"></i>
+                </HeaderButtons>
+                <HeaderButtons>
+                    <i className="fa-regular fa-bell"></i>
+                </HeaderButtons>
+                <HeaderButtons>
+                    <i className="fa-regular fa-circle-user"></i>
+                </HeaderButtons>
             </section>
         </header>
     )
